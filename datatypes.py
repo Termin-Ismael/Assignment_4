3c9ad3e230c20056e752da1a0a635b3fcfa6fa06
Termin-Ismael <termin.ismael@gmail.com>
2023-07-29 22:09:32 -0400

    adding files

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+first_name = "Termin"
+age = 28
+weight = 135.5
+type_first_name = type(first_name)
+type_age = type(age)
+type_weight = type(weight)
+print(f"the first name of the user is {first_name}, and the data type of first_name is {type_first_name}")
+print(f"the age of the user is {age}, and the data type of age is {type_age}")
+print(f"the weight of the user is {weight}, and the data type of weight is {type_weight}")
---
+++ termin.py
@@ -0,0 +1,5 @@
+first_name = "Termin"
+middle_name = "Elmelda"
+last_name = "Ismael"
+full_name = first_name + " " + middle_name + " " + last_name
+print(full_name)
